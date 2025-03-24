bcc28d61df554dd72e16dd0b59b06c731dbc7ee3
Diogo Soares <diogosoares@cliente-200-135-65-111.wireless.ufsc.br>
2025-03-24 09:20:00 -0300

    feat: Add Depth First Search to the graph repository. TODO: Eulerian Graph Cicle implementation

--- graphs/WeightedUndirectedGraph/GraphSearch.cpp
+++ graphs/WeightedUndirectedGraph/GraphSearch.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <optional>
 #include <algorithm>
+#include <stack>
 #include "WeightedUndirectedGraph.h"
 #include "GraphSearch.h"
 
@@ -28,12 +29,12 @@ std::vector<size_t> BreadthFirstSearch(const WeightedUndirectedGraph<Tp>& graph,
     Q.push(startVertexIndex);
 
     while (!Q.empty()) {
-        size_t u = Q.front();
+        size_t u = Q.front();  // Gets a 1-based index
         Q.pop();
 
         // Loop through neighbors, adjust index if needed
         for (auto vertex : graph.vizinhos(graph.getVertex(u))) {
-            size_t v = vertex->index - 1;  // Convert to 0-based index
+            size_t v = vertex->index - 1;  // Convert to 0-based index for array operations
 
             if (!knownVertices[v]) {  // If the current vertex is not known
                 knownVertices[v] = true;
@@ -78,11 +79,42 @@ std::vector<size_t> BreadthFirstSearch(const WeightedUndirectedGraph<Tp>& graph,
     return traveledDistance;
 }
 
-template std::vector<size_t> BreadthFirstSearch<std::string>(const WeightedUndirectedGraph<std::string>&, size_t);
-
-
-
 template<typename Tp>
-std::vector<size_t> DepthFirstSearch(const WeightedUndirectedGraph<Tp>& graph, Tp startVertexIndex) {
+std::vector<size_t> DepthFirstSearch(const WeightedUndirectedGraph<Tp>& graph, size_t startVertexIndex) {
+    // Configuring all vertices
+    size_t n {graph.qtdVertices()};
 
+    std::vector<bool> knownVertices(n, false);
+    std::vector<size_t> timeElapsed(n, std::numeric_limits<size_t>::max());
+    std::vector<std::optional<size_t>> predecessorVertices(n, std::nullopt);
+
+    // Configuring the source vertex (correct the index for 0-based array access)
+    knownVertices[startVertexIndex - 1] = true;
+    size_t currentTime {0};
+
+    // Preparing the visitor stack
+    std::stack<size_t> S;
+    S.push(startVertexIndex);
+
+    while (!S.empty()) {
+        currentTime += 1;
+        size_t u {S.top()};
+        S.pop();
+        timeElapsed[u-1] = currentTime  // Converting to 0-index based
+
+        for (auto vertex : graph.vizinhos(graph.getVertex(u))) {
+            size_t v = vertex->index - 1;  // Converting to 0-based index for array operations
+
+            if (!knownVertices[v]) {
+                knownVertices[v] = true;
+                predecessorVertices[v] = u - 1  // 'u' is 1-index based, so we are converting to 1-index based
+                S.push(v + 1);  // Push back as 1-indexed for consistency in getVertex calls
+            }
+        }
+    }
+
+    return timeElapsed;
 }
+
+template std::vector<size_t> BreadthFirstSearch<std::string>(const WeightedUndirectedGraph<std::string>&, size_t);
+template std::vector<size_t> DepthFirstSearch<std::string>(const WeightedUndirectedGraph<std::string>&, size_t);
